f0f46bd878fbd36db854c6d53e7c1c7639e99247
Jorge de Souza <jorgejrdj1@gmail.com>
2021-09-18 22:49:38 -0300

    Configurações de atualizações automáticas

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -9,6 +11,14 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  devServer: {
+    static: path.resolve(__dirname, "public")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   module: {
     rules: [
       {
