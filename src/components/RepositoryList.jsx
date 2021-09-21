0f25deb1556c318e64e93a12ee6dfc0ecd4eb399
Jorge de Souza <jorgejrdj1@gmail.com>
2021-09-21 20:44:25 -0300

    props

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,13 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <RepositoryItem />
+      </ul>
+    </section>
+  )
+}
