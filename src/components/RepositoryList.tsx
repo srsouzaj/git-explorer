3cef11d8b1059e50891e09037659d70fc670ad1b
Jorge de Souza <jorgejrdj1@gmail.com>
2021-09-20 21:51:00 -0300

    Primeiros componentes

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,17 @@
+const repositoryName = 'unform';
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React</p>
+          <a href=""> Acessar repositório </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
