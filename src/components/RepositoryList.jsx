50830bc03460fa47324879b271b0b7e9ec45f420
Jorge de Souza <jorgejrdj1@gmail.com>
2021-09-21 22:07:57 -0300

    estilização

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss'
+
 const repository = {
   name: 'unform',
   description: 'Form in React',
@@ -12,6 +14,8 @@ export function RepositoryList() {
       <h1>Lista de Repositórios</h1>
 
       <ul>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
         <RepositoryItem repository={repository} />
       </ul>
     </section>
